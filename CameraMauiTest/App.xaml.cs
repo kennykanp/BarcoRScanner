291c61c53facd8c077dc74cd2c302ba031542ff7
kenny.nino <knino@t-idsolutions.com>
2024-03-19 14:59:13 -0500

    MauiCamera base

---
+++ CameraMauiTest/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CameraMauiTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
